1b6212d244f6045bb5ca5a1c6736e03659685303
JH0-cyber <maric020485@gmail.com>
2025-06-02 07:47:14 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function quetapiweb() {
-  window.location = "https://sites.google.com/view/quetapitonstudio-br-/in%C3%ADcio"
+  window.location = "https://sites.google.com/view/quetapitonstudio-br-/in%
+ADcio"
+}
+function OAM(){
+   window.location.href = "https://gamipt.onrender.com/oam.html"
 }
